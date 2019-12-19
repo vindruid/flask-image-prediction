780c5f6e9e48296933c576ab5bb5f2ae04ed2225
hervind phil <hervindruid@gmail.com>
2019-12-19 14:52:37 +0700

    upload and show image

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-import os 
-
 from flask import Flask,render_template,url_for,request,g, flash, redirect
 from werkzeug.utils import secure_filename
 
+import os 
+import cv2
+
 ## Upload parameter
-UPLOAD_FOLDER = 'uploads'
+UPLOAD_FOLDER = os.path.join('static', 'uploaded_images')
 ALLOWED_EXTENSIONS = ['jpg', 'jpeg', 'png']
 
 ## FLASK configuration
@@ -16,14 +17,25 @@ def allowed_file(filename):
     return '.' in filename and \
            str(filename.rsplit('.', 1)[1]).lower() in ALLOWED_EXTENSIONS
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route('/')
 def home_page():
+    return render_template('page.html')
+
+@app.route('/a')
+def heal():
+    return render_template('page.html')
+
+@app.route('/prediction', methods=['GET', 'POST'])
+def predict():
     ### UPLOAD FILE
     if 'uploadfile' in request.files.keys():
         submission_file = request.files['uploadfile']
         #throw error if extension is not allowed
         if not allowed_file(submission_file.filename):
-            raise Exception('Invalid file extension')
+            message = "Only Accept jpg, jpeg and png format"
+            img_path = '.'
+
 
         elif submission_file:
             filename = secure_filename(submission_file.filename)
@@ -32,11 +44,23 @@ def home_page():
             if not os.path.exists(target_dir):
                 os.makedirs(target_dir)
             
-            fullPath = os.path.join(app.config['UPLOAD_FOLDER'] , filename)
-            submission_file.save(fullPath)
+            img_path = os.path.join(".", app.config['UPLOAD_FOLDER'] , filename)
+            submission_file.save(img_path)
+
+            img = cv2.imread(img_path)
+            prediction = img.shape[0]
+
+            message = f"Model prediction: {prediction}"
+
+    else: 
+        message = "No Images Uploaded"
+        img_path = '.'
                 
-            return redirect(url_for('home_page'))
-    return render_template('index.html')
+    return render_template('page.html',
+                            message = message, 
+                            img_path = img_path,
+    )
+                            
 
 
 if __name__ == '__main__':
